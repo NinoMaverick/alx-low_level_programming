ae208f1cf905b122da662004efe5ded648f9288c
NinoMaverick <etengyemmy@gmail.com>
2023-10-05 20:15:11 -0700

    c4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
- /* This program prints a message using the puts function. */
- puts("Programming is like building a multilingual puzzle");
+ /*This program prints a message using the puts function. */
+ puts("\"Programming is like building a multilingual puzzle");
  return (0);
 }
 
