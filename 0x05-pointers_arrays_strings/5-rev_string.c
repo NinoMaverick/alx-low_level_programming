949e41322fd8f8caa58c49706de59a56fe7a9be2
NinoMaverick <etengyemmy@gmail.com>
2023-10-18 10:11:39 -0700

    0x05-5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,36 +3,20 @@
 /**
  * rev_string - function that reverses a string
  * @s: character string that would be reversed
- * start - String starting point
- * end - String ending point
- * length - shows length of string
  * Return: Always 0
  */
 void rev_string(char *s)
 {
-	if (s == NULL)
+	int a;
+	int count = 0;
+	char swap;
+
+	for (a = 0; s[a] != '\0'; a++)
+		count++;
+	for (a = 0; a < count / 2; a++)
 	{
-		return;
-	}
-
-	int length = 0;
-	int start = 0;
-	int end = -1;
-
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-
-	while (start < end)
-	{
-		temp = s[start];
-
-		s[start] = s[end];
-
-		s[end] = temp;
-
-		start++;
-		end--;
+		swap = s[a];
+		s[a] = s[count - 1 - a];
+		s[count - 1 - a] = swap;
 	}
 }
