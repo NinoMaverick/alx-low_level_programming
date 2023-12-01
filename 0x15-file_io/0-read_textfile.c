7e7ad503df636a9bbe9c9570769c020aab317e1c
NinoMaverick <etengyemmy@gmail.com>
2023-12-01 05:26:27 -0800

    0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,30 +12,29 @@
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
-{
-	ssize_t o, r, w;
-	char *buffer;
+{	
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
-	return (0);
+	if (!filename)
+		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer ==  NULL)
-	return (0);
+	fd = open(filename, O_RDONLY);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	if (fd == -1)
+		return (0);
 
-	if (o == -1  r == -1  w == -1 || w != r)
-	{
-	free(buffer);
-	return (0);
-	}
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-	free(buffer);
-	close(o);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	return (w);
+	close(fd);
 
+	free(buf);
+
+	return (nwr);
 }
